18dc87b8ac7f7ee8b3f5ec2ffc457142e0d590a2
Gisela Gomez <gisela865504@gmail.com>
2022-09-01 19:39:03 -0300

    estilo

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,11 +4,12 @@ import './Item.css'
 const Item = ({ detalle }) => {
     
   return (            
-            <div className="col-12 mb-2 col-md-4">
+            <div className="container">
                 <div className="card">
                     <img src={detalle.images} alt={detalle.name} className="card-img-top"></img> 
                     <div className="card-body">
                        <h4>{detalle.name}</h4>
+                       <h6>{detalle.description}</h6>
                        <p>${detalle.price}</p>
                        <span>Stock Disponible: {detalle.stock}</span>
                        <button className="btn btn-primary" type="button">Comprar</button>
