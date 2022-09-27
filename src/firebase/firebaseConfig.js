da1618bd541dc3f51757afd98324d508b075795a
Gisela Gomez <gisela865504@gmail.com>
2022-09-27 13:29:03 -0300

    firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEre0ACi5733ETw1xxbC1O24KVU5tMVmU",
+  authDomain: "ecommerce38060.firebaseapp.com",
+  projectId: "ecommerce38060",
+  storageBucket: "ecommerce38060.appspot.com",
+  messagingSenderId: "856619150869",
+  appId: "1:856619150869:web:6bdee4e6ac605b5676281c"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
